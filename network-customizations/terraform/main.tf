1093c995c16d1c79ba80f40ac3382922ba747bdf
EC2 Default User <ec2-user@ip-192-168-0-131.ec2.internal>
2024-04-11 22:30:03 +0000

    Update vpc module version2

--- network-customizations/terraform/main.tf
+++ network-customizations/terraform/main.tf
@@ -1,4 +1,4 @@
-module "vpc_example_simple-vpc" {
-  source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.1"
 }
