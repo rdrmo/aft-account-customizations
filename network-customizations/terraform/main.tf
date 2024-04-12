daac1fb87fb1c84b37d67000eff24c139909c3be
EC2 Default User <ec2-user@ip-192-168-0-131.ec2.internal>
2024-04-12 14:19:13 +0000

    try7

--- network-customizations/terraform/main.tf
+++ network-customizations/terraform/main.tf
@@ -3,3 +3,18 @@ module "vpc" {
   version = "5.7.1"
 }
 
+data "aws_availability_zones" "available" {}
+
+locals {
+  name   = "Transit"
+  region = "us-east-1"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-vpc"
+    GithubOrg  = "terraform-aws-modules"
+  }
+}
