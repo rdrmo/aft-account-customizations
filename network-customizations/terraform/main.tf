5839c4302df9699faf5e02f19abe8317eff0c783
EC2 Default User <ec2-user@ip-192-168-0-131.ec2.internal>
2024-04-12 14:02:04 +0000

    try6

--- network-customizations/terraform/main.tf
+++ network-customizations/terraform/main.tf
@@ -1,34 +1,5 @@
-### Transit infrastructure
-
-data "aws_availability_zones" "available" {
-  state = "available"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.7.1"
 }
 
-data "aws_organizations_organization" "org" {}
-
-locals {
-  name   = "Hub"
-  region = var.region
-}
-
-################################################################################
-# VPC section
-################################################################################
-
-#
-## Main VPC
-#
-resource "aws_vpc" "transit" {
-  cidr_block            = var.cidr_block
-  instance_tenancy      = "default"
-  
-  enable_dns_support    = true
-  enable_dns_hostnames  = true
-
-  tags = {
-	#Name                = var.vpc_name
-	Name                = "TransitVPC"
-	Project             = "SharedNetwork"
-	CostCenter          = "Network"
-  }
-}
